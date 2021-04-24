e2b385695a1a867fc1ffc6df53bf545bd6b27488
Samith Shetty <sshett15@uncc.edu>
2021-04-24 12:19:59 -0400

    Changed upcoming assignment command to ouput in Embed format

--- cogs/canvasCog.py
+++ cogs/canvasCog.py
@@ -4,13 +4,14 @@ from canvasapi import Canvas
 from canvasapi.course import Course, Assignment
 import datetime
 import pytz
+from time import perf_counter
 from config import CANVAS_TOKEN
 
 API_URL = 'https://uncc.instructure.com'
 
 EST = pytz.timezone('US/Eastern')
 CANVAS_DATE_FORMAT = r'%Y-%m-%dT%H:%M:%SZ'
-OUTPUT_DATE_FORMAT = r'%a, %b %d at %I:%M %p'
+OUTPUT_DATE_FORMAT = r'%A, %b %d at %I:%M %p'
 
 class CanvasCog(commands.Cog):
     def __init__(self,bot):
@@ -24,6 +25,7 @@ class CanvasCog(commands.Cog):
     
     @commands.command(aliases = ["due", "upcoming", "assignments"])
     async def getAssignments(self, ctx, course_code, num_assignments = 5):
+        start_time = perf_counter()
         course = self.canvas.get_course(course_code)
         sorted_assignments = sorted(course.get_assignments(),key = lambda a: a.due_at if a.due_at is not None else " ")
         current_time = datetime.datetime.now()
@@ -35,13 +37,18 @@ class CanvasCog(commands.Cog):
         #Take slice of user-specified size (or until end of list, if that comes sooner)
         upcoming_assignments = sorted_assignments[assignment_index:min(assignment_index+num_assignments,len(sorted_assignments))]
         
-        message = f"The upcoming assignments due in {course.name} are:\n"
+        embed = discord.Embed(
+            title = course.name,
+            description = "Here are the upcoming due dates for this course:"
+        )
         for assignment in upcoming_assignments:
-            due_date = datetime.datetime.strptime(assignment.due_at, CANVAS_DATE_FORMAT) #Convert from str to datetime object
+            due_date = pytz.utc.localize(datetime.datetime.strptime(assignment.due_at, CANVAS_DATE_FORMAT)) #Convert from str to datetime object
             due_date_est = due_date.astimezone(EST)#convert from default UTC to EST
-            message += f'{assignment.name}' + ' \u200b' * max(0,80 - len(assignment.name)) + f' {due_date_est.strftime(OUTPUT_DATE_FORMAT)}\n'
-        await ctx.send(message)
-        print(f"Successfully sent upcoming assignments for {course.name}")
+            embed.add_field(name = assignment.name, value = due_date_est.strftime(OUTPUT_DATE_FORMAT), inline=False)
+        response_time = perf_counter() - start_time
+        embed.set_footer(text= f"Response time: {response_time:1.3}")
+        await ctx.send(embed=embed)
+        print(f"Successfully sent upcoming assignments for {course.name:<80} {response_time:1.3}")
     
 def setup(bot):
     bot.add_cog(CanvasCog(bot))
