cf5e37c775569d52e9a5f8e9b308f0aa7df5356c
Samith Shetty <samithshetty0@gmail.com>
2022-03-28 16:41:48 -0400

    fix: updated intents and bot start

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import discord
 from discord.ext import commands
+import asyncio
 import os
 from config import DISCORD_TOKEN
 
-intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
-bot = commands.Bot(command_prefix=">", intents=intents)
+intents = discord.Intents.default()
+intents.message_content=True
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 
 @bot.event
 async def on_ready():
     print('Successfully logged in and booted')
 
-
 bot.remove_command("help")
 
-# Automatically load all the cogs on startup
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        try:
-            bot.load_extension(f'cogs.{filename[:-3]}')
-        except Exception as e:
-            print(f'Error loading cog {filename}:\n{e}')
+async def main():
+    # Automatically load all the cogs on startup
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            try:
+                await bot.load_extension(f'cogs.{filename[:-3]}')
+            except Exception as e:
+                print(f'Error loading cog {filename}:\n{e}')
+    await bot.start(DISCORD_TOKEN)
 
-bot.run(DISCORD_TOKEN)
+asyncio.run(main())
